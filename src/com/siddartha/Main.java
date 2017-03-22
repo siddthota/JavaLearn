c373bd221133c62e8515467755de5c9d77c9c63e
Siddartha Thota <th.siddu@gmail.com>
2017-03-21 21:13:13 -0400

    Update Main.java

--- src/com/siddartha/Main.java
+++ src/com/siddartha/Main.java
@@ -26,6 +26,15 @@ public class Main {
         // Method OverLoading - The class to have two or more methods having same name, if their argument lists are different.
         calcFeetAndInchesToCentimeters(5, 10);
         calcFeetAndInchesToCentimeters(65);
+
+        //Polymorphism - An operation may exhibit different behavior in different instances
+
+        for(int i=1; i<11; i++) {
+            Movie movie = randomMovie();
+            System.out.println("Movie #" + i +
+                    " : " + movie.getName() + "\n" +
+                    "Plot: " + movie.plot() + "\n");
+        }
     }
 
     public static double calcFeetAndInchesToCentimeters(int feet, int inches) {
@@ -53,4 +62,99 @@ public class Main {
             return -1;
         }
     }
+
+
+    //Polymorphism -
+
+    public static Movie randomMovie() {
+        int randomNumber = (int) (Math.random() * 5) +1;
+        System.out.println("Random number generated was: " + randomNumber);
+        switch (randomNumber) {
+            case 1:
+                return new Jaws();
+            case 2:
+                return new IndependenceDay();
+            case 3:
+                return new MazeRunner();
+            case 4:
+                return new StarWars();
+            case 5:
+                return new Forgetable();
+        }
+
+        return null;
+    }
 }
+
+class Movie {
+    private String name;
+
+    public Movie(String name) {
+        this.name = name;
+    }
+
+    public String plot() {
+        return "No plot here";
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+
+class Jaws extends Movie {
+    public Jaws() {
+        super("Jaws");
+    }
+
+    public String plot() {
+        return "A shark eats lots of people";
+    }
+}
+
+class IndependenceDay extends Movie {
+
+    public IndependenceDay() {
+        super("Independence Day");
+    }
+
+    @Override
+    public String plot() {
+        return "Aliens attempt to take over planet earth";
+    }
+}
+
+class MazeRunner extends Movie {
+
+    public MazeRunner() {
+        super("Maze Runner");
+    }
+
+    @Override
+    public String plot() {
+        return "Kids try and escape a maze";
+    }
+}
+
+class StarWars extends Movie {
+    public StarWars() {
+        super("Star Wars");
+    }
+
+    @Override
+    public String plot() {
+        return "Imperial Forces try to take over the universe";
+    }
+}
+
+class Forgetable extends Movie {
+    public Forgetable() {
+        super("Forgetable");
+    }
+
+    // No plot method
+}
+
+
+
+
